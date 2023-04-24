6527458ddae7a978daf0d513410b4493a2b46855
killcraft <killcraft1122999@gmail.com>
2023-04-24 13:06:11 +0700

    function add

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -1,4 +1,22 @@
-﻿
+﻿class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN;
+
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+
+            default:
+                break;
+        }
+        return result;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
@@ -35,12 +53,25 @@ class Program
             }
 
             Console.WriteLine("Choose an operator from the following list:");
-            
+            Console.WriteLine("\ta - Add");
             Console.Write("Your option? ");
 
             string op = Console.ReadLine();
 
-           
+            try
+            {
+                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+
             Console.WriteLine("------------------------\n");
 
             Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
